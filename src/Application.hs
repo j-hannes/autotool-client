17fbc556769ac595bcb1d1440b0a164506c0f49b
J-Hannes <Hannes_E@gmx.de>
2012-12-17 19:37:59 +0000

    Remove unneeded skeleton files

--- src/Application.hs
+++ src/Application.hs
@@ -9,14 +9,10 @@ module Application where
 import Control.Lens
 import Snap.Snaplet
 import Snap.Snaplet.Heist
-import Snap.Snaplet.Auth
-import Snap.Snaplet.Session
 
 ------------------------------------------------------------------------------
 data App = App
     { _heist :: Snaplet (Heist App)
-    , _sess :: Snaplet SessionManager
-    , _auth :: Snaplet (AuthManager App)
     }
 
 makeLenses ''App
@@ -27,5 +23,3 @@ instance HasHeist App where
 
 ------------------------------------------------------------------------------
 type AppHandler = Handler App App
-
-
